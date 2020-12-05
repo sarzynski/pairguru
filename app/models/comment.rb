e4e06ce55eaaf2341c4dec89677fcd669ac7bea0
sarzynski <kamil.s.sarzynski@gmail.com>
2020-12-05 17:34:24 +0100

    add comment controller and view

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :movie
   belongs_to :user
+  validates :movie_id, uniqueness: { scope: :user_id, message: "You can leave only one comment to the movie" }
 end

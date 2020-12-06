50ce131ca6a2f37af04d96f07b2136f1c0067c85
sarzynski <kamil.s.sarzynski@gmail.com>
2020-12-06 21:38:51 +0100

    add more tests checking counting method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,15 +9,24 @@ describe User do
   it { is_expected.not_to allow_value("+48 999 888 777\naseasd").for(:phone_number) }
   it { is_expected.to have_many(:comments) }
 
-  it 'returns empty array if no comments' do
+  it 'returns empty array if there are no comments' do
     expect(described_class.count_comments).to be_empty
+  end
+
+  it 'returns empty array if the only one comment is older that 7 days' do
+    create(:comment, created_at: 8.days.ago)
+    expect(described_class.count_comments).to be_empty
+  end
+
+  it 'returns one comment if the only one comment was posted 7 days ago' do
+    create(:comment, created_at: 7.days.ago)
+    expect(described_class.count_comments.to_a.size).to eq(1)
   end
 
   it 'returns only top 10 commenters' do
     11.times do
       create(:comment)
     end
-    top_commenters = described_class.count_comments.to_a
-    expect(top_commenters.size).to eq(10)
+    expect(described_class.count_comments.to_a.size).to eq(10)
   end
 end

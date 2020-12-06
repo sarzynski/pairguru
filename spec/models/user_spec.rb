a37b8c303f4a7f2b2f28dcf07c51a68bfd3f2f5e
sarzynski <kamil.s.sarzynski@gmail.com>
2020-12-06 15:09:32 +0100

    add tests and remove extra files

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,8 @@ describe User do
   it { is_expected.not_to allow_value("aaa +48 aaa bbb ccc").for(:phone_number) }
   it { is_expected.not_to allow_value("+48 999 888 777\naseasd").for(:phone_number) }
   it { is_expected.to have_many(:comments) }
+
+  it 'returns empty array if no comments' do
+    expect(described_class.count_comments).to be_empty
+  end
 end

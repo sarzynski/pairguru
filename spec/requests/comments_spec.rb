2388b764b9bfbe77cad554932830b828b2a36364
sarzynski <kamil.s.sarzynski@gmail.com>
2020-12-07 14:07:57 +0100

    update comments requests test to make it more precise

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -2,12 +2,12 @@ require "rails_helper"
 
 describe "Comments requests", type: :request do
   let!(:movie) { create(:movie) }
-  let!(:comment) { create(:comment, movie_id: movie.id)}
+  let!(:comment) { create(:comment, content: "Sample comment", movie_id: movie.id)}
 
   describe "comments list" do
     it "displays comment" do
       visit "/movies/#{movie.id}"
-      expect(page).to have_content("commented")
+      expect(page).to have_content("Sample comment")
     end
   end
 end
